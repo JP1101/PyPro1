034d9c21a8941c3ed5f731fab110d672df551b22
JP1101 <111163298+JP1101@users.noreply.github.com>
2023-10-07 12:20:00 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesion como {bot.user}')
+
+@bot.command()
+async def add(ctx, number1:float, number2:float):
+    await ctx.send(number1 + number2)
+
+@bot.command()
+async def subtract(ctx, number1:float, number2:float):
+    await ctx.send(number1 - number2)
+
+@bot.command()
+async def multiply(ctx, number1:float, number2:float):
+    await ctx.send(number1 * number2)
+
+@bot.command()
+async def split(ctx, number1: float, number2: float):
+    await ctx.send(number1 / number2)
+
+
+equationslist = {
+    "$add ",
+    "$subtract ",
+    "$multiply ",
+    "$split "
+}
+@bot.command()
+async def equations(ctx):
+    await ctx.send(f"These are the equations that you can do with this bot {equationslist} ")
+
+bot.run("MTE1NjM4NTIwMTE3NDM2NDIwMQ.Gsq5c7.IX9VUqVkXMJZYAvPhOYlbUG2pOLiv_hXS8qEqs")
